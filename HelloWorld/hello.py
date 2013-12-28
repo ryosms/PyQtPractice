c93cef8ef879ce93b819e03116bf538ed21cf06c
ryosms <ryo.sms@gmail.com>
2013-12-29 01:56:05 +0900

    Add method for click event

--- HelloWorld/hello.py
+++ HelloWorld/hello.py
@@ -2,11 +2,17 @@ import sys
 from PyQt4 import QtCore, QtGui
 
 
-def main():
+def on_click():
+    print("Hello World")
 
+
+def main():
     app = QtGui.QApplication(sys.argv)
     main_window = QtGui.QMainWindow()
     hello_button = QtGui.QPushButton(u"波浪ワールド")
+
+    hello_button.clicked.connect(on_click)
+
     main_window.setCentralWidget(hello_button)
     main_window.show()
     app.exec()
