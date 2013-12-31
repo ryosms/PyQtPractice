cd4e9b77fddd6d198fa101d9078510475f5db1bd
ryosms <ryo.sms@gmail.com>
2013-12-31 23:13:36 +0900

    Add "An applicaiton icon" in "First programs"

---
+++ ZetCodeTutorial/01_FirstPrograms/application_icon.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle("Icon")
+        self.setWindowIcon(QtGui.QIcon("qt.png"))
+
+        self.show()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+
