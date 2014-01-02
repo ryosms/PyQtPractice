09bb0b5ab2d51896d0f65e9d647dd0ba4e99d6aa
ryosms <ryo.sms@gmail.com>
2014-01-02 15:50:39 +0900

    Add "Centering window on the screen" in "First programs"

---
+++ ZetCodeTutorial/01_FirstPrograms/centering_window.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.resize(250, 150)
+        self.center()
+
+        self.setWindowTitle("Center")
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
