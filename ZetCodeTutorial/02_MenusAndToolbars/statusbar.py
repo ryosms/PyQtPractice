518ab012149027cbc717b813ac89f69e2d4e2d02
ryosms <ryo.sms@gmail.com>
2014-01-02 16:48:42 +0900

    Add "Statusbar" in "Menus and Toolbars"

---
+++ ZetCodeTutorial/02_MenusAndToolbars/statusbar.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.statusBar().showMessage("Ready")
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle("Statusbar")
+        self.show()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
