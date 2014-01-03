527a656ae9e70fd8b5c5fcb836a2cdd2fff33fb7
ryosms <ryo.sms@gmail.com>
2014-01-03 23:30:23 +0900

    Add "QtGui.QColorDialog" in "Dialogs"

---
+++ ZetCodeTutorial/05_Dialogs/color_dialog.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        col = QtGui.QColor(0, 0, 0)
+
+        self.btn = QtGui.QPushButton("Dialog", self)
+        self.btn.move(20, 20)
+
+        self.btn.clicked.connect(self.show_dialog)
+
+        self.frm = QtGui.QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }" % col.name())
+        self.frm.setGeometry(130, 22, 100, 100)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle("Color dialog")
+        self.show()
+
+    def show_dialog(self):
+        col = QtGui.QColorDialog.getColor()
+
+        if col.isValid():
+            self.frm.setStyleSheet("QWidget { background-color: %s }" % col.name())
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
