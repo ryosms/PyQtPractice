a146b7211ae9e84890cabd1cf1ad45513ecaf8d2
ryosms <ryo.sms@gmail.com>
2014-01-04 23:20:56 +0900

    Add "Simple Drag and Drop" in "Drag and Drop"

---
+++ ZetCodeTutorial/08_DragAndDrop/simple_drag_and_drop.py
@@ -0,0 +1,45 @@
+import sys
+from PyQt4 import QtGui
+
+
+class Button(QtGui.QPushButton):
+    def __init__(self, title, parent):
+        super(Button, self).__init__(title, parent)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        if e.mimeData().hasFormat("text/plain"):
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+        self.setText(e.mimeData().text())
+
+
+class Example(QtGui.QWidget):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        edit = QtGui.QLineEdit("", self)
+        edit.setDragEnabled(True)
+        edit.move(30, 65)
+
+        button = Button("Button", self)
+        button.move(190, 65)
+
+        self.setWindowTitle("Simple Drag & Drop")
+        self.setGeometry(300, 300, 300, 150)
+        self.show()
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
